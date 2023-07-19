ebf6b778848c3d75d791de8c996375cdad611ce5
chiraze <ajroudichiraze@gmail.com>
2023-07-19 18:02:39 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ProductName from './Component/ProductName';
+import ProductPrice from './Component/ProductPrice';
+import ProductDescription from './Component/ProductDescription';
+import ProductImage from './Component/ProductImage';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Product Details</h1>
+      <ProductName />
+      <ProductPrice />
+      <ProductDescription />
+      <ProductImage />
     </div>
   );
-}
+};
 
 export default App;
